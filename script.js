87b9700cc3d43f7a96688d815fc9eeaa3ab670af
ritamroy18 <ritamroycs07@gmail.com>
2020-04-01 17:09:48 +0530

    changes

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 var select = document.getElementById("test");
 var options = select.children;
 console.log(options);
-// var text = stateSelect.options[stateSelect.selectedIndex].text;
-
-var node = document.createElement('div');
-node.classList.add('select-dropdown__button');
-select.parentElement.appendChild(node);
-
 var ul = document.createElement('ul');
 ul.classList.add('select-dropdown');
+var li = document.createElement('li');
+li.classList.add('select-dropdown-item');
 
 var data = '';
 for (var i = 0; i < options.length; i++) {
-    data += '<li class="select-dropdown-item" data-value="' + options[i].value + '">' + options[i].textContent + '</li>'
+    data += '<li  data-value="' + options[i].value + '">' + options[i].textContent + '</li>'
     console.log( options[i].textContent)
 }
-
+// li.appendChild = data;
 ul.innerHTML = data;
 select.parentElement.append(ul);
+
 
 //select
 //option
