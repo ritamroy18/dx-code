9bcd5348db0d653fb51b7fe97fc2e1cf0ab003ba
ritamroy18 <62829549+ritamroy18@users.noreply.github.com>
2020-04-02 14:03:56 +0530

    foreach
    foreach

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 var select = document.getElementById("test");
-var options = select.children;
+var options = Array.from(select.children);
 console.log(options);
 var ul = document.createElement('ul');
 ul.classList.add('select-dropdown');
-var li = document.createElement('li');
+var li = document.createElement('li');    //Create Element with li
 li.classList.add('select-dropdown-item');
 
 var data = '';
-for (var i = 0; i < options.length; i++) {
-    data += '<li  data-value="' + options[i].value + '">' + options[i].textContent + '</li>'
-    console.log( options[i].textContent)
-}
+// for (var i = 0; i < options.length; i++) {
+//     data += '<li  data-value="' + options[i].value + '">' + options[i].textContent + '</li>'
+//     console.log( options[i].textContent)
+// }
+
+options.forEach((value, index) => {
+    console.log(`i value: ${index} | Value:`, value.textContent);
+    data += '<li  data-value="' + index + '">' + value.textContent + '</li>'
+});
+
 // li.appendChild = data;
 ul.innerHTML = data;
 select.parentElement.append(ul);
+
 
 
 //select
