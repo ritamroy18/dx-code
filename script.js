1c7c5f87b47e434b815e4f709ae1529b37c72512
ritamroy18 <ritamroycs07@gmail.com>
2020-04-07 15:19:22 +0530

    select and ui hide and input create with select value

--- script.js
+++ script.js
@@ -2,30 +2,46 @@
 var select = document.querySelectorAll('.test'); //Select by Class 
 console.log(select);
 
+
+
+
 // for (var i = 0; i < select.length; i++) {
 select.forEach((value) => {
-// var options = Array.from(select[i].children);
-var options = Array.from(value.children);
+
+    var selectedText = value.options[value.selectedIndex].text;
+    console.log(selectedText);
+    // var divCreate = document.createElement('div');
+    // var span = document.createElement('span');
+    var input = document.createElement('input');
+
+    input.value = selectedText;
+    // span.innerText = selectedText
+    // divCreate.innerHTML= selectedText;
+    // value.parentElement.append(input);
+    value.parentElement.append(input);
+    input.style.display= 'block';
+    // divCreate.style.display= 'block';
+    value.style.display = 'none';   
+    
+
+var options = Array.from(value.children).slice(1);
 console.log(options);
+
+
 
 var ul = document.createElement('ul');
 ul.classList.add('select-dropdown');
 
-
 var data = '';
-// for (var i = 0; i < options.length; i++) {
-//     data += '<li  data-value="' + options[i].value + '">' + options[i].textContent + '</li>'
-//     console.log( options[i].textContent)
-// }
 
 options.forEach((value, index) => {
-    // console.log(`i value: ${index} | Value:`, value.textContent);
-    // data += '<li  data-value="' + index + '">' + value.textContent + '</li>'
     data = value.textContent;
     var li = document.createElement('li');    //Create Element with li
     li.setAttribute("data-value", index);
     li.appendChild(document.createTextNode(data));
     ul.appendChild(li);
+    ul.style.display = 'none'; //add display none to ul
+    
 });
 // select[i].parentElement.append(ul);
 value.parentElement.append(ul);
@@ -41,6 +57,14 @@ value.parentElement.append(ul);
 //ul elemnt
 //option loop
 //append to li
+
+
+
+//new option select
+//create div
+//selcted value in the div
+//ulhide
+ 
 
 
 
